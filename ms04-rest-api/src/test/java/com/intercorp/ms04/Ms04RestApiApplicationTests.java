6fa58cd02ddff4d451f3ca4aaba2d4e259849935
Leonardo <56612664+LeonardoSGSG@users.noreply.github.com>
2025-06-14 23:35:52 -0500

    Fix: convertir ms04-rest-api de submódulo a carpeta común

---
+++ ms04-rest-api/src/test/java/com/intercorp/ms04/Ms04RestApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.intercorp.ms04;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms04RestApiApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
